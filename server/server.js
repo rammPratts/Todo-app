653cd2a6896cbe35f73d25ed1a0104b951125c8f
Ramiro Meneses Prattes <xanco78@gmail.com>
2018-08-28 18:51:00 -0300

    Añadido POST /user y tokens

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb");
@@ -86,6 +88,17 @@ app.patch("/todos/:id",(req,res) => {
             res.send({todo});
         }).catch((e) => res.status(400).send());
     }
+});
+
+app.post("/users",(req,res) => {
+    var body = _.pick(req.body,["email","password"]);
+    var user = new User(body);
+    
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header("x-auth",token).send(user);
+    }).catch((e) => res.status(400).send(e));
 });
 
 app.listen(port,()=>{
