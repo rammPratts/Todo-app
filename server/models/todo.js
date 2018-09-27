b5dde8fd654a13ea088b603297840af7a376cf6f
Ramiro Meneses Prattes <xanco78@gmail.com>
2018-09-26 21:29:50 -0300

    Added private routes with authentication

--- server/models/todo.js
+++ server/models/todo.js
@@ -14,6 +14,11 @@ var Todo = mongoose.model("Todos",{
     completedAt: {
         type: Number,
         default: null
+    },
+
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     }
 });
 
